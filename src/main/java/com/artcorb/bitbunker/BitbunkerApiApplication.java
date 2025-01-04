2bf3d0e0d089d9cd43806f15eb4e449498b7f1d2
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-01-03 21:29:13 -0300

    first commit

---
+++ src/main/java/com/artcorb/bitbunker/BitbunkerApiApplication.java
@@ -0,0 +1,13 @@
+package com.artcorb.bitbunker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbunkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbunkerApiApplication.class, args);
+	}
+
+}
