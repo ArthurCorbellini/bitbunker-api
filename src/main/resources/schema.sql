bc209ac53727fb63a270f3d6c9b3eb8f05f28913
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-02-24 19:52:37 -0300

    chore: dados fictícios para dev

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -29,3 +29,16 @@ CREATE TABLE IF NOT EXISTS `orders` (
 
   FOREIGN KEY (`asset_id`) REFERENCES `assets`(`id`)
 );
+
+INSERT INTO `assets` (`ucid`, `name`, `symbol`, `type`, `classification`, `created_at`, `created_by`, `updated_at`, `updated_by`)  
+VALUES 
+  (1, 'Bitcoin', 'BTC', 'CRYPTO', 'TIER_S', '2025-02-24', 'admin', NULL, NULL),
+  (2, 'Solana', 'SOL', 'CRYPTO', 'TIER_A', '2025-02-24', 'admin', NULL, NULL),
+  (3, 'Ethereum', 'ETH', 'CRYPTO', 'TIER_A', '2025-02-24', 'admin', NULL, NULL),
+  (null, 'Real', 'BRL', 'FIAT', 'NO_TIER', '2025-02-24', 'admin', NULL, NULL);
+
+INSERT INTO `orders` (`asset_id`, `type`, `quantity`, `brl_quantity`, `notes`, `created_at`, `created_by`, `updated_at`, `updated_by`)  
+VALUES 
+  (1, 'BUY', 0.0050000, 150000.0000000, 'Compra de Bitcoin', '2025-02-24', 'admin', NULL, NULL),
+  (1, 'SELL', 0.0020000, 100000.0000000, NULL, '2025-02-24', 'admin', NULL, NULL),
+  (3, 'DEPOSIT', 5000, 5000, NULL, '2025-02-24', 'admin', NULL, NULL);
