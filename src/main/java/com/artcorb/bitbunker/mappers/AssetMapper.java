483ef430e9dfe7fadc8a80da1055af4f0f332fd3
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-02-28 22:14:19 -0300

    fix: ajusta o mapper do assets

--- src/main/java/com/artcorb/bitbunker/mappers/AssetMapper.java
+++ src/main/java/com/artcorb/bitbunker/mappers/AssetMapper.java
@@ -4,24 +4,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 import com.artcorb.bitbunker.dtos.AssetDto;
 import com.artcorb.bitbunker.enums.AssetRank;
+import com.artcorb.bitbunker.enums.AssetType;
 import com.artcorb.bitbunker.models.Asset;
 
 public class AssetMapper {
 
   public static Asset toEntity(AssetDto source) {
     Asset target = new Asset();
+    target.setId(source.getId());
     target.setUcid(source.getUcid());
     target.setName(source.getName());
     target.setSymbol(source.getSymbol());
+    target.setType(AssetType.valueOf(source.getType()));
     target.setClassification(AssetRank.valueOf(source.getClassification()));
     return target;
   }
 
   public static AssetDto toDto(Asset source) {
     AssetDto target = new AssetDto();
+    target.setId(source.getId());
     target.setUcid(source.getUcid());
     target.setName(source.getName());
     target.setSymbol(source.getSymbol());
+    target.setType(source.getType().toString());
     target.setClassification(source.getClassification().toString());
     return target;
   }
